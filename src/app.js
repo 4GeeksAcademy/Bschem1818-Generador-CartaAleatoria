c7b5300483bc3982c9a38af35d860e045ae5a2c3
Bschem1818 <barbaraschem@gmail.com>
2024-08-20 00:14:57 +0000

    upadted

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const num = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const symbols = ["♦", "♥", "♠", "♣"];
+  const color = ["red", "black"];
+  let numAleatorio = Math.floor(Math.random() * num.length);
+  let symbolsAleatorio = Math.floor(Math.random() * symbols.length);
+  let colorAleatorio = Math.floor(Math.random() * color.length);
+  let espacioFiguras = document.querySelectorAll("#symbols");
+  espacioFiguras.forEach(element => {
+    element.innerText = symbols[symbolsAleatorio];
+    element.style.color = color[colorAleatorio];
+  });
+  let espacioDigit = document.getElementById("digit");
+  espacioDigit.innerText = num[numAleatorio];
 };
